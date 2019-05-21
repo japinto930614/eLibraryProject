db8dfffd4201498d6de6dc5c1aa1963d208c97e1
RodrigueMedor <rodriguemedor@yahoo.fr>
2019-05-21 12:13:38 -0500

    Creation class of Book

--- src/main/java/com/elibrary/mum/project/model/Book.java
+++ src/main/java/com/elibrary/mum/project/model/Book.java
@@ -1,4 +1,66 @@
 package com.elibrary.mum.project.model;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.function.LongBinaryOperator;
+
+@Entity
+@Table(name = "BOOKs")
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long BookId;
+    private String title;
+    private String ISBN;
+    private String Author;
+    private String Description;
+    @NotNull(message = "* Date supplied is required")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dateAdd;
+
+    public Book() {
+    }
+
+    public Long getBookId() {
+        return BookId;
+    }
+
+    public void setBookId(Long bookId) {
+        BookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public void setAuthor(String author) {
+        Author = author;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
 }
