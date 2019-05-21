11efdb36644516132297b82850e08f1f89f34153
RodrigueMedor <rodriguemedor@yahoo.fr>
2019-05-21 13:17:15 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/elibrary/mum/project/service/IBookCopyService.java

--- src/main/java/com/elibrary/mum/project/service/ICategoryService.java
+++ src/main/java/com/elibrary/mum/project/service/ICategoryService.java
@@ -1,4 +1,17 @@
 package com.elibrary.mum.project.service;
 
+import com.elibrary.mum.project.model.Book;
+import com.elibrary.mum.project.model.Category;
+
+import java.util.List;
+
 public interface ICategoryService {
+    String createPost(Book book);
+    boolean removeCategory(Category category);
+    List<Category> listOfCategory();
+    Category findOneCategory(Long id);
+
+
+
+
 }
--- src/main/java/com/elibrary/mum/project/service/ILocationService.java
+++ src/main/java/com/elibrary/mum/project/service/ILocationService.java
@@ -1,4 +1,12 @@
 package com.elibrary.mum.project.service;
 
+import com.elibrary.mum.project.model.Location;
+
+import java.util.List;
+
 public interface ILocationService {
+    String createPost(Location book);
+    boolean removeCategory(Location location);
+    List<Location> listOfCategory();
+    Location findOneCategory(Long id);
 }
