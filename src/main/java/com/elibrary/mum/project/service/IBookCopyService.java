87433005669d5fcbd8de883edf03ba460219720e
RodrigueMedor <rodriguemedor@yahoo.fr>
2019-05-21 13:00:59 -0500

    Creation class of Book

--- src/main/java/com/elibrary/mum/project/service/IBookCopyService.java
+++ src/main/java/com/elibrary/mum/project/service/IBookCopyService.java
@@ -1,4 +1,6 @@
 package com.elibrary.mum.project.service;
 
 public interface IBookCopyService {
+
+
 }
--- src/main/java/com/elibrary/mum/project/service/IBookService.java
+++ src/main/java/com/elibrary/mum/project/service/IBookService.java
@@ -1,4 +1,14 @@
 package com.elibrary.mum.project.service;
 
+import com.elibrary.mum.project.model.Book;
+
+import java.util.List;
+
 public interface IBookService {
+     void addBook();
+     List<Book> getListOfBook();
+     boolean removeOfBook(Book book);
+     Book findOneBook(Long id);
+    void updateBook(Book book);
+
 }
