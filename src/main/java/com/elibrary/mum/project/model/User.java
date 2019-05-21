8f9cb4f3e6d635496891fa8e009e522fd773fb7e
Nguezang Nsaha <nsahaarsene@yahoo.com>
2019-05-21 12:08:59 -0500

    "Created User attributes"

--- src/main/java/com/elibrary/mum/project/model/User.java
+++ src/main/java/com/elibrary/mum/project/model/User.java
@@ -1,4 +1,69 @@
 package com.elibrary.mum.project.model;
 
+import java.time.LocalDate;
+
 public class User {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String email;
+    private LocalDate registrationDate;
+    private double overduefine;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public double getOverduefine() {
+        return overduefine;
+    }
+
+    public void setOverduefine(double overduefine) {
+        this.overduefine = overduefine;
+    }
 }
--- src/main/java/com/elibrary/mum/project/model/UserType.java
+++ src/main/java/com/elibrary/mum/project/model/UserType.java
@@ -1,4 +1,27 @@
 package com.elibrary.mum.project.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserType {
+    private Long userTypeId;
+    private String userTypeName;
+    private List<User> users = new ArrayList<User>();
+
+    public Long getUserTypeId() {
+        return userTypeId;
+    }
+
+    public void setUserTypeId(Long userTypeId) {
+        this.userTypeId = userTypeId;
+    }
+
+    public String getUserTypeName() {
+        return userTypeName;
+    }
+
+    public void setUserTypeName(String userTypeName) {
+        this.userTypeName = userTypeName;
+    }
+
 }
