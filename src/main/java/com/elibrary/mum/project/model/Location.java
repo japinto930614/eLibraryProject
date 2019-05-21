624adf0ba22038539e99049a09fb5972aabeb148
Nguezang Nsaha <nsahaarsene@yahoo.com>
2019-05-21 13:18:33 -0500

    "Created Location and implemented IBookService"

--- src/main/java/com/elibrary/mum/project/model/Location.java
+++ src/main/java/com/elibrary/mum/project/model/Location.java
@@ -1,7 +1,46 @@
 package com.elibrary.mum.project.model;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "LOCATIONS")
 public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int locationId;
+
+    private int section;
+    private int row;
+
+    public Location() {
+
+    }
+
+    public Location(int section, int row) {
+
+    }
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
+
+    public int getSection() {
+        return section;
+    }
+
+    public void setSection(int section) {
+        this.section = section;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
 }
