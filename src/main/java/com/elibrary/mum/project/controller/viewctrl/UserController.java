97c0f83be049afb4df7d4873bbc425e68bbf7cd8
jorge <jorge_930614@hotmail.com>
2019-05-21 17:04:19 -0500

    User interface work

--- src/main/java/com/elibrary/mum/project/controller/viewctrl/UserController.java
+++ src/main/java/com/elibrary/mum/project/controller/viewctrl/UserController.java
@@ -3,6 +3,8 @@ package com.elibrary.mum.project.controller.viewctrl;
 
 
 import com.elibrary.mum.project.model.User;
+import com.elibrary.mum.project.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,38 +15,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import java.util.List;
 
 
 @Controller
 @RequestMapping(value = "/eLibraryFinal/secured/user")
 public class UserController {
 
-//    @Autowired
-//    private ISupplierService supplierService;
+    @Autowired
+    private IUserService userService;
 
     @GetMapping(value = "/browse")
-    public ModelAndView displayListOfSuppliers() {
+    public ModelAndView displayListOfUsers() {
         ModelAndView modelAndView = new ModelAndView();
-//        List<Supplier> suppliers = supplierService.getAllSuppliers();
-//        modelAndView.addObject("suppliers", suppliers);
+        List<User> users = userService.getAllUsers();
+        modelAndView.addObject("users", users);
         modelAndView.setViewName("secured/user/browse");
         return modelAndView;
     }
 
     @GetMapping(value = "/new")
-    public String newSupplierForm(Model model) {
-//        model.addAttribute("user", new Supplier());
+    public String newUserForm(Model model) {
+        model.addAttribute("user", new User());
         return "secured/user/new";
     }
 
     @PostMapping(value = "/new")
-    public String addNewSupplier(@Valid @ModelAttribute("user") User user,
+    public String addNewUser(@Valid @ModelAttribute("user") User user,
                                  BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
             model.addAttribute("errors", bindingResult.getAllErrors());
             return "secured/user/new";
         }
-//        user = supplierService.addNewSupplier(user);
+        user = userService.addUser(user);
         return "redirect:/srm/secured/user/browse";
     }
 
